dfc37b6e2edc9be08369f7d9f58ac11bdc4cad46
PureJoyMind <mazdak.parniandg@gmail.com>
2023-04-30 16:08:08 +0430

    Add project files.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // To have applicationDbContext work with the database
+        private readonly ApplicationDbContext _db;
+
+        // Then we need to tell the application that we need an implementation of _db
+        // where the connection to db is already made
+        public CategoryController(ApplicationDbContext dbContext) 
+        {
+            _db = dbContext;
+        }
+        public IActionResult Index()
+        {
+            // Retrieving objects from db
+            IEnumerable<Category> objectCategoryList = _db.Categories; // Can also use .ToList() with var
+            return View(objectCategoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Category());
+        }
+    }
+}
